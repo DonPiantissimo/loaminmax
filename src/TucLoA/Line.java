c43350a3bf2da0c4911b7470cc5e2fa680c5f1fc
DonPiantissimo <DonPiantissimo@hotmail.com>
2016-07-06 10:23:00 +0300

    Update Line.java

--- src/TucLoA/Line.java
+++ src/TucLoA/Line.java
@@ -15,6 +15,14 @@ public class Line {
   public double value_neg = 1;
   public double value;
   
+  public double diag_pos_value;
+  public double diag_neg_value;
+  
+  public double diag_value;
+  
+  private boolean pos_end = false;
+  private boolean neg_end = false;
+  
   public Line(int order, byte BOARD_SIZE, byte EMPTY, Trinary[] formation){
     this.order = order;
     this.BOARD_SIZE = BOARD_SIZE;
@@ -28,7 +36,7 @@ public class Line {
 
     findMoveDistance();
     value=getValue();
-
+    diag_value = getDiagValues();
    
   }
   
@@ -39,15 +47,17 @@ public class Line {
         moveDistance++;
   }
   
-  public int getValue(){
+  public double getValue(){
     if (order + moveDistance >= BOARD_SIZE)
       value_pos=0;
     else {
       if (formation[order+moveDistance]==SELF)
         value_pos=0;
       else {
-        if (order+moveDistance==BOARD_SIZE-1)
+        if (order+moveDistance==BOARD_SIZE-1){
           value_pos*=0.5;
+          pos_end=true;
+        }
         for (int i=order+1;i<order+moveDistance;i++)
           if (formation[order]==OPP){
             value_pos=0;
@@ -59,24 +69,38 @@ public class Line {
       value_pos*=2;
       
     if (order - moveDistance < 0)
-      value_pos=0;
+      value_neg=0;
     else {
       if (formation[order-moveDistance]==SELF)
-        value_pos=0;
+        value_neg=0;
       else {
-        if (order-moveDistance==0)
-          value_pos*=0.5;
+        if (order-moveDistance==0){
+          value_neg*=0.5;
+          neg_end=true;
+        }
         for (int i=order-1;i>order-moveDistance;i--)
           if (formation[order]==OPP){
-            value_pos=0;
+            value_neg=0;
             break;
           }
         }
     }
     if (formation[order-moveDistance]==OPP)
-      value_pos*=2;
+      value_neg*=2;
     
     return value_pos+value_neg;
+  }
+  
+  public double getDiagValues(){
+    if (pos_end)
+      diag_pos_value = value_pos*0.5;
+    else diag_pos_value = value_pos;
+    
+    if (neg_end)
+      diag_neg_value = value_neg*0.5;
+    else diag_neg_value = value_neg;
+    
+    return diag_pos_value+diag_neg_value;
   }
   
 }
