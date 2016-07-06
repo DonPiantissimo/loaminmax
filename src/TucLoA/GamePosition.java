8e84404e8b6ed593b5621f8af4a93cc227604e10
DonPiantissimo <DonPiantissimo@hotmail.com>
2016-07-06 08:51:35 +0300

    Update GamePosition.java

--- src/TucLoA/GamePosition.java
+++ src/TucLoA/GamePosition.java
@@ -7,9 +7,13 @@ public class GamePosition {
 	final byte WHITE = 0;
 	final byte BLACK = 1;
 	final byte EMPTY = 2;
+
+	final int NUMBER_OF_PIECES = 12;	
+	private int sumOfMinimalDistances[];
 	
 	public GamePosition() {
 		board = new byte[BOARD_SIZE][BOARD_SIZE];
+		sumOfMinimalDistancesInit();
 	}
 	
 	public GamePosition(byte def){
@@ -35,5 +39,14 @@ public class GamePosition {
 	
 	public void setTurn(byte t) {
 		turn = t;
+	}
+	
+	public void sumOfMinimalDistancesInit(){
+		sumOfMinimalDistances = new int[NUMBER_OF_PIECES+1];
+		sumOfMinimalDistances[0]=-1
+		for (int i=1;i<=9;i++)
+			sumOfMinimalDistances[i]=sumOfMinimalDistances[i-1]+1;
+		for (int i=10;i<=14 && i<=NUMBER_OF_PIECES;i++)
+			sumOfMinimalDistances[i]=sumOfMinimalDistances[i-1]+2;
 	}
 }
