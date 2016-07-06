6d360c644f3dc0e64043303fa68d1b5680c8d6c4
DonPiantissimo <DonPiantissimo@hotmail.com>
2016-07-06 09:49:20 +0300

    Update Trinary.java

--- src/TucLoA/Trinary.java
+++ src/TucLoA/Trinary.java
@@ -1,5 +1,21 @@
 package TucLoA;
 
 public class Trinary {
-
+  public byte digit;
+  private Trinary next;
+  private final byte LIMIT=3;
+  
+  public Trinary (byte digit, Trinary next){
+    this.digit = digit;
+    this.next = next;
+  }
+  
+  public byte increment(){
+    digit++;
+    if (digit==LIMIT){
+      digit=0;
+      next.increment();
+    }
+    return digit;
+  }
 }
